ca6ee5a12865b47392755a211f458f41655a8dcf
Emanuel Marques <emanuelaluno.ti@gmail.com>
2024-05-21 20:14:40 -0300

    contador aula

---
+++ main.py
@@ -0,0 +1,8 @@
+#Usuário informa um número
+
+contador = int(input('Informe um número inteiro: '))
+
+#conta a partir do número informado até 0
+while contador >= 0:
+    print(contador)
+    contador -= 1
